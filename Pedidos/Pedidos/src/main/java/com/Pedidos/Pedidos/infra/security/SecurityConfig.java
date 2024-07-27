fd7cacd6676d8b0c3bace409c7ae49fb1cfe106a
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-07-27 12:15:28 -0300

    update

--- Pedidos/Pedidos/src/main/java/com/Pedidos/Pedidos/infra/security/SecurityConfig.java
+++ Pedidos/Pedidos/src/main/java/com/Pedidos/Pedidos/infra/security/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")  // Restrição para admin
+                        .requestMatchers("/swagger-ui/**", "/v2/api-docs/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
